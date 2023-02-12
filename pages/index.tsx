10608548163631e04f6e1cf7248cf5c9f7dfb7b3
José Manuel Domínguez <68101685+jmdc89@users.noreply.github.com>
2023-02-12 16:19:39 +0300

    bugfix
    tailwindcss

---
+++ pages/index.tsx
@@ -0,0 +1,18 @@
+import type {NextPage} from "next";
+import Head from 'next/head'
+
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Jmdc portfolio</title>
+      </Head>
+
+      <h1 className="text-blue-800">Lets build an awesome portfolio</h1>
+    </div>
+
+  )
+}
+
+export default Home;
