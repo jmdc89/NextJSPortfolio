6bc6567be1d2979055a5d7e3bd85ddc95c73e3be
José Manuel Domínguez <68101685+jmdc89@users.noreply.github.com>
2023-02-13 13:52:34 +0300

    Update Header.tsx
    darkmode test

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,34 +1,49 @@
 import React from 'react';
 import { SocialIcon } from 'react-social-icons';
+import useColorMode from "../hooks/useColorMode";
 
 type Props = {}
 
 export default function Header({}: Props) {
+
+  const [colorMode, setColorMode] = useColorMode();
+
   return (
-    <header>
+    <header className="sticky top-0 flex items-start justify-between">
       <div className="flex flex-row items-center">
-      {/* Social Icons */}
+        {/* Social Icons */}
         <SocialIcon 
         url="https://www.linkedin.com/in/jos%C3%A9-manuel-dom%C3%ADnguez-425b1b113/"
-        fgColor="gray"
+        fgColor={ colorMode === "dark" ? "gray" : "black" }
         bgColor="transparent"
         />
         <SocialIcon 
         url="https://github.com/jmdc89" 
-        fgColor="gray"
+        fgColor={ colorMode === "dark" ? "gray" : "black" }
         bgColor="transparent"
         />
       </div>
 
-      <div>
+      <div className='flex'>
         <SocialIcon
         className="cursor-pointer"
         network="email"
-        fgColor="gray"
+        fgColor={ colorMode === "dark" ? "gray" : "black" }
         bgColor="transparent"
         />
         <p className="uppercase hidden md:inline-flex text-sm text-gray-400">Get in Touch</p>
-      </div>
+        
+        <div className="flex bg-white dark:bg-black">
+        <button
+          className="dark:text-white text-black bg-white dark:bg-black"
+          onClick={() => setColorMode(colorMode === "light" ? "dark" : "light")}
+        >
+          Tog
+        </button>
+         </div>
+
+    </div>
+    
     </header>
   )
 }
