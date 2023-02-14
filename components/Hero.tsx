d9b42a2b351479e1bfff301db055a97b0d9fa69b
José Manuel Domínguez <68101685+jmdc89@users.noreply.github.com>
2023-02-14 10:39:44 +0300

    Update Hero.tsx
    typewriter section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from 'react';
+import { Cursor, useTypewriter} from 'react-simple-typewriter'
 
 type Props = {}
 
 export default function Hero({}: Props) {
+
+    const [text, count] = useTypewriter({
+
+        words: [
+            "Hi, The name' s Jose",
+            "Computational Designer",
+            "<AI Lover />",
+        ],
+        loop: true,
+        delaySpeed: 2000,
+    })
+
   return (
-    <div>Hero</div>
+    <div>
+        <h1>
+            <span>{text}</span>
+            <Cursor cursorColor="blue" />
+        </h1>
+    </div>
   )
 }
