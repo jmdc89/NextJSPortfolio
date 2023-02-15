d6118eb83135ca7b8d3554ea82d166d308f25d42
José Manuel Domínguez <68101685+jmdc89@users.noreply.github.com>
2023-02-15 14:19:04 +0300

    Update _app.tsx
    ThemeProvider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,14 +2,12 @@ import '@/styles/globals.css'
 import 'tailwindcss/tailwind.css';
 import type { AppProps } from 'next/app'
 import React, { useState, useEffect } from "react";
-import { ThemeProvider } from "next-themes";
+
 
 
 export default function App({ Component, pageProps }: AppProps) {
 
   return (
-    <ThemeProvider enableSystem={true} attribute="class">
         <Component {...pageProps} />
-    </ThemeProvider>
   );
 }
