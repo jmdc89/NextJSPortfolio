57a386276e829d9cd7b50ee5b9b395d4d72d0ba7
José Manuel Domínguez <68101685+jmdc89@users.noreply.github.com>
2023-02-15 08:35:36 +0300

    Update _app.tsx
    setDarkMode useeffect

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,23 @@
 import '@/styles/globals.css'
 import 'tailwindcss/tailwind.css';
 import type { AppProps } from 'next/app'
+import { useState, useEffect } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
+
+  const [darkMode, setDarkMode] = useState(false);
+
+  useEffect(() => {
+    if (document.body) {
+      if (darkMode) {
+        document.body.classList.add("dark");
+      } else {
+        document.body.classList.remove("dark");
+      }
+    }
+  }, [darkMode]);
+
   return (
-        <Component {...pageProps} />
+        <Component {...pageProps} darkMode={darkMode} setDarkMode={setDarkMode} />
   );
 }
